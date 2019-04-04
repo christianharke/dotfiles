6beaf015ed375a826ef8573217f732acacb9fb07
Christian Harke <ch.harke@gmail.com>
2019-04-05 01:04:21 +0200

    Make install script parameterizable
    Signed-off-by: Christian Harke <ch.harke@gmail.com>

--- install.sh
+++ install.sh
@@ -2,8 +2,25 @@
 
 cd "$(dirname "$0")" || exit
 
-find . -maxdepth 1 -type d -name "[a-z]*" -print | while read f;
-do
-    echo "Install $(basename "$f") ..."
-    stow -t "$HOME" -R "$(basename "$f")"
-done
+function install() {
+    echo "Install $(basename "$1") ..."
+    stow -t "$HOME" -R "$(basename "$1")"
+}
+readonly -f install
+
+if [ -z "$1" ];
+then
+    echo "Usage: ./install.sh [--all] folder ..."
+    exit 1
+elif [[ $1 = "--all" ]];
+then
+    find . -maxdepth 1 -type d -name "[a-z]*" -print | while read -r f;
+    do
+        install "$f"
+    done
+else
+    for i in "$@";
+    do
+        install "$i"
+    done
+fi
