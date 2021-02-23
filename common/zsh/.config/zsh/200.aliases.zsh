7a7f743ce4ad501bf5044829ea2bd4e080ea3a1c
Christian Harke <christian.harke@bluecare.ch>
2021-02-23 20:58:16 +0100

    Add cat and grep aliases

--- common/zsh/.config/zsh/200.aliases.zsh
+++ common/zsh/.config/zsh/200.aliases.zsh
@@ -1,22 +1,22 @@
 # ALIASES
 
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-alias -g ......='../../../../..'
-alias -g .......='../../../../../..'
-alias -g ........='../../../../../../..'
+alias -g ...="../.."
+alias -g ....="../../.."
+alias -g .....="../../../.."
+alias -g ......="../../../../.."
+alias -g .......="../../../../../.."
+alias -g ........="../../../../../../.."
 
-alias d='dirs -v | head -10'                            # List the last ten directories we've been to this session, no duplicates
+alias d="dirs -v | head -10"                            # List the last ten directories we've been to this session, no duplicates
 
+alias cat="bat"
+alias grep="rg"
 alias l="exa -hl --git --icons"
 alias la="exa -ahl --git --icons"
-alias ll='l'
+alias ll="l"
 alias ls="ls -sh --color='auto'"
-alias lsa='ls -a'
-alias tree='l --tree'
-
-alias grep="grep --color=auto"                          # Color grep - highlight desired sequence
+alias lsa="ls -a"
+alias tree="l --tree"
 
 alias fa="fontawesome-menu -f icon-list.txt"            # Open Fontawesome icon selector
 alias jshell="nix-shell -p openjdk --command jshell"    # Java REPL
